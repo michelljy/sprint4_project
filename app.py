d3bf58c4a870e5a417bb5be4eaffc9a76ee55256
michelljy <michelljy@gmail.com>
2025-04-05 16:13:57 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,16 +2,20 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
+# Load your dataset
 df = pd.read_csv('vehicles_us.csv')
 
+# Add a header
 st.header('Car Sales Dashboard')
 
+# Add a checkbox
 show_histogram = st.checkbox('Show Histogram')
 
+# Conditional display of the histogram
 if show_histogram:
     fig_hist = px.histogram(df, x='model_year', title='Distribution of Car Model Years')
-st.plotly_chart(fig_hist)
-   
-
+    st.plotly_chart(fig_hist)
+    
+# Scatterplot display
 fig_scatter = px.scatter(df, x='price', y='odometer', title='Price vs. Odometer')
 st.plotly_chart(fig_scatter)
