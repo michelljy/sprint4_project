36baa010120b449f116bf4716d04b382599bb6aa
michelljy <michelljy@gmail.com>
2025-04-05 16:05:48 -0700

    Update app.py

--- app.py
+++ app.py
@@ -6,12 +6,12 @@ df = pd.read_csv('vehicles_us.csv')
 
 st.header('Car Sales Dashboard')
 
-fig_hist = px.histogram(df, x='model_year', title='Distribution of Car Model Years')
+show_histogram = st.checkbox('Show Histogram')
+
+if show_histogram:
+    fig_hist = px.histogram(df, x='model_year', title='Distribution of Car Model Years')
 st.plotly_chart(fig_hist)
+   
 
 fig_scatter = px.scatter(df, x='price', y='odometer', title='Price vs. Odometer')
 st.plotly_chart(fig_scatter)
-
-if st.checkbox('Show Raw Data'):
-    st.dataframe(df)
-
